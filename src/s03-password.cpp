e0ec91612cfce71133452880ad02ada033f9edd4
Maciej Zagorski <s23575@pjwstk.edu.pl>
2020-11-07 12:05:47 +0100

    Exercise: "03-loops.pdf", page 23

---
+++ src/s03-password.cpp
@@ -0,0 +1,25 @@
+/*
+ * Ask the user for their credentials. Allow them to decline.
+ */
+
+
+#include <iostream>
+#include <string>
+
+
+auto main() -> int
+{
+
+   auto name = std::string{};
+
+   do {
+
+        std::cout << "password:";
+        std::getline(std::cin, name);
+        
+    } while (name != "student");
+
+    std::cout << "ok!\n";
+
+   return 0;
+}
