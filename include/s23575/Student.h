d16dee54c24ea55c02e2f3140d5ee820b427bf6f
Maciej Zagorski <s23575@pjwstk.edu.pl>
2020-11-21 13:15:37 +0100

    Exercise: "04-data_structures.pdf", p. 24

---
+++ include/s23575/Student.h
@@ -0,0 +1,29 @@
+#ifndef S23575_STUDENT_H
+#define S23575_STUDENT_H
+
+#include <sstream>
+#include <string>
+
+namespace s23575 {
+
+struct Student {
+    std::string imie;
+    std::string nazwisko;
+    std::string indeks;
+    constexpr static unsigned SEMESTR = 1;
+    unsigned semestr{SEMESTR};
+    constexpr static double SREDNIA = 0.00;
+    double srednia{SREDNIA};
+
+    Student(std::string,
+            std::string,
+            std::string,
+            unsigned const = SEMESTR,
+            double const   = SREDNIA);
+
+    auto to_string() const -> std::string;
+};
+
+}  // namespace s23575
+
+#endif  // S23575_STUDENT_H
---
+++ src/s03-Student.cpp
@@ -0,0 +1,38 @@
+#include <s23575/Student.h>
+
+#include <iostream>
+
+s23575::Student::Student(std::string im,
+                         std::string naz,
+                         std::string ind,
+                         unsigned const sem,
+                         double const sred)
+        : imie{std::move(im)}
+        , nazwisko{std::move(naz)}
+        , indeks{std::move(ind)}
+        , semestr{sem}
+        , srednia{sred}
+{
+    std::cout << "Utworzono studenta.\n";
+}
+
+auto s23575::Student::to_string() const -> std::string
+{
+    auto out = std::ostringstream{};
+    out << "Dane studenta:\n"
+        << "Imię: " << imie << "\n"
+        << "Nazwisko: " << nazwisko << "\n"
+        << "Indeks: " << indeks << "\n"
+        << "Semestr: " << semestr << "\n"
+        << "Średnia: " << srednia;
+    return out.str();
+}
+
+auto main() -> int
+{
+    auto student = s23575::Student("Maciej", "Zagórski", "s23575", 1, 3.25);
+
+    std::cout << student.to_string() << "\n";
+
+    return 0;
+}
